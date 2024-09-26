22eb2b977b66d457c883243f4fbbb2db32bc0efa
HERNAN BENAVIDES <hernand.benavidesc@uqvirtual.edu.co>
2024-09-25 23:57:09 -0500

    Update EventoController.java
    mejoras en los endpoints

--- src/main/java/co/edu/uniquindio/unieventosbackend/controllers/EventoController.java
+++ src/main/java/co/edu/uniquindio/unieventosbackend/controllers/EventoController.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.unieventosbackend.controllers;
 
+import co.edu.uniquindio.unieventosbackend.dto.EventoGetDTO;
 import co.edu.uniquindio.unieventosbackend.dto.EventoListarDTO;
 import co.edu.uniquindio.unieventosbackend.dto.evento.CrearEventoDTO;
 import co.edu.uniquindio.unieventosbackend.model.documents.Evento;
@@ -10,41 +11,44 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/unieventos/eventos")
+@RequestMapping("/uni-eventos")
 public class EventoController {
 
      @Autowired
      private EventoService eventoService;
 
 
-     @PostMapping("/crear")
-     public ResponseEntity<?> crearEvento(@Valid @RequestBody CrearEventoDTO evento) {
-          return ResponseEntity.ok(eventoService.crearEvento(new Evento(evento)));
-     }
-
-     @GetMapping("/eventos-disponibles")
+     @GetMapping("/eventos")
      public ResponseEntity<Page<EventoListarDTO>> obtenerEventosDisponibles(@PageableDefault(size =5) Pageable pageable) {
           return ResponseEntity.ok(eventoService.obtenerEventosDisponibles(pageable).map(EventoListarDTO::new));
      }
 
-     @GetMapping("/eventos-disponibles/{id}")
+     @GetMapping("/eventos/{id}")
      public ResponseEntity<?> obtenerEventoDisponible(@PathVariable String id) {
           Optional<Evento> evento = Optional.ofNullable(eventoService.findById(id));
           if (evento.isEmpty()) {
                return ResponseEntity.notFound().build();
           }
-          return ResponseEntity.ok(evento);
+          return ResponseEntity.ok(new EventoGetDTO(evento.get()));
+     }
+
+
+
+     @PostMapping("/eventos/crear")
+     @PreAuthorize("hasRole('ADMINISTRADOR')")
+     public ResponseEntity<?> crearEvento(@Valid @RequestBody CrearEventoDTO evento) {
+          return ResponseEntity.ok(eventoService.crearEvento(new Evento(evento)));
      }
 
      @DeleteMapping("/eliminar/{id}")
-     @Transactional
+     @PreAuthorize("hasRole('ADMINISTRADOR')")
      public ResponseEntity<?> eliminarEvento(@PathVariable String id){
           Evento evento = eventoService.eliminarEvento(id);
           if(evento.getIsActivo()){
